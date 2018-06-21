0ef62a5b25d811c4ce6bff984c815fb1337452c0
Junseop Park <metternichch@gmail.com>
2018-06-21 17:55:16 +0900

    react redux

--- src/index.js
+++ src/index.js
@@ -1,45 +1,12 @@
-import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
-import VideoList from './components/video_list';
-import SearchBar from './components/search_bar';
-import VideoDetail from './components/video_detail';
-
-const API_KEY = 'AIzaSyARL4aEJ_u8h42IjWYldvcpyemM28_rj4Q';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      videos : [],
-      selectedVideo: null
-    };
-
-    this.videoSearch('surfboards');
-  }
-
-  videoSearch(term){
-    YTSearch({key: API_KEY, term: term}, (videos) => {
-      this.setState({
-        videos : videos,
-        selectedVideo : videos[0]
-      });
-    });
-  }
-
   render(){
-    const videoSearch = _.debounce((term) => {this.videoSearch(term)},300)
-
 
     return (
       <div>
-        <SearchBar onSearchTermChange={videoSearch} />
-        <VideoDetail video={this.state.selectedVideo}/>
-        <VideoList
-        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
-        videos={this.state.videos}/>
+          React simple starter
       </div>
     );
   }
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function (){
+  return [
+    {title: 'javascript: The Good Parts'},
+    {title: 'javascript: Harry Potter'},
+    {title: 'javascript: The Dark Tower'},
+    {title: 'javascript: Eloquent Ruby'}
+  ]
+}
